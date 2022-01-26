903c15f8f6751562f46da4cc616eb9266851ed12
Matthew Rogers <infinate1234flight@gmail.com>
2022-01-26 09:51:47 -0800

    Made more things toggleable. Changed title

--- myscript.js
+++ myscript.js
@@ -1,9 +1,31 @@
+var numtoggle = 0;
+
 $(document).ready(function() {
 	$("h1").click(function() {
+		if(numtoggle == 0)
+		{
 		$("p").css({"color":"red", "background-color": "yellow"});
 		$("img").css("border","25px solid black");
+		numtoggle = 1;
+		}
+	 else if(numtoggle == 1)
+		{
+		$("p").css({"color":"DarkBlue", "background-color": "lightblue"});
+		$("img").css("border","none");
+		numtoggle = 0;
+		}
 	});
 	$("#imagetoggler").click(function(){
 		$("img").toggle();
+	});
+	$("#p1").click(function(){
+		$("#p2").toggle();
+		$("#p3").toggle();
+	});
+	$("img").hover(function(){
+		$("img").css({"width":"50%"});
+	},
+		function(){
+    $("img").css({"width":"20%"});
 	});
 });
